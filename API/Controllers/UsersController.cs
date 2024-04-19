2159354f277db30164d3f92a399a305e78a680ba
ripcody <ripcody@gmail.com>
2024-04-19 18:27:03 -0400

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,33 +1,37 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")] // GET /api/users
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
-  private readonly DataContext _context;
-
-  public UsersController(DataContext context)
+  [Authorize]
+  public class UsersController : BaseApiController
   {
-    _context = context;
-  }
+    private readonly DataContext _context;
 
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-  {
-    var users = await _context.Users.ToListAsync();
+    public UsersController(DataContext context)
+    {
+      _context = context;
+    }
 
-    return users;
-  }
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+      var users = await _context.Users.ToListAsync();
 
-  [HttpGet("{id}")]
+      return users;
+    }
 
-  public async Task<ActionResult<AppUser>> GetUser(int id)
-  {
-    return await _context.Users.FindAsync(id);
+    [HttpGet("{id}")]
+
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
   }
 }
+
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,28 +1,29 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
-internal class Program
-{
-  private static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
+// Add services to the container.
 
-    builder.Services.AddControllers();
-    builder.Services.AddDbContext<DataContext>(opt =>
-    {
-      opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    });
-    builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-    app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
-    app.Run();
-  }
-}
+app.MapControllers();
+
+app.Run();
+
