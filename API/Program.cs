4736c5aa618010101d07fabed6b3791420ff1f09
ripcody <ripcody@gmail.com>
2024-07-31 19:53:21 -0400

    End of section 16

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
 using System.Reflection.Metadata.Ecma335;
 using API.Data;
+using API.Entities;
 using API.Extensions;
 using API.Middleware;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,8 +32,10 @@ var services = scope.ServiceProvider;
 try
 {
   var context = services.GetRequiredService<DataContext>();
+  var userManager = services.GetRequiredService<UserManager<AppUser>>();
+  var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
   await context.Database.MigrateAsync();
-  await Seed.SeedUsers(context);
+  await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
 {
